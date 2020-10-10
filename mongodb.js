a731db3f5b08d5d5c281c72f4de3b30355f32b57
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-10 12:58:59 +0300

    findOne and find to read documents from the mongo database

--- mongodb.js
+++ mongodb.js
@@ -1,78 +1,47 @@
 // CRUD OPERATIONS (create, read, updata, delete)
 
-// // importing the module we installed
-// const mongodb = require("mongodb");
-// // to initialize the connection. The MongoClient
-// const MongoClient = mongodb.MongoClient;
-// const ObjectID = mongodb.ObjectID;
-
 const { MongoClient, ObjectID } = require('mongodb');
 
-const id = new ObjectID(); // Generates a new id for us. 
-console.log(id); // The first 4 bytes give the timestamp.
-console.log(id.id); // the binary format
-console.log(id.id.length); // 12
-console.log(id.toHexString().length); // 24 double the size, that's why we use binary data.
-console.log(id.getTimestamp()); 
-
-// The connection url (localhost) and the database we are trying to connect to. 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-app";
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
+
     if (error) {
         return console.log("Unable to connect to database!");
     }
-    // The database is created automatically and we get a reference.
+
     const db = client.db(databaseName);
 
-    // // insertOne is asynchronous, we insert just one document
-    // db.collection("users").insertOne({
-    //     // _id: id, // We can provide the _id if we want
-    //     name: "Vikram",
-    //     age: 26
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log("Unable to insert user.");
-    //     }
-    //     // ops --> array of documents
-    //     console.log(result.ops);
-    // });
+    // Find an indivindual document (not finding one returns null | if there are multiple documents returns the first one back)
 
-    // db.collection("users").insertMany([
-    //     {
-    //         name: "Jen",
-    //         age: 28
-    //     },
-    //     {
-    //         name: "Gunter",
-    //         age: 27
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log("Unable to insert documents!");
-    //     }
-    //     console.log(result.ops);
-    // });
+    db.collection("users").findOne({ _id: new ObjectID("5f817f5864bffe47d824febf") }, (error, user) => {
+        if (error) {
+            return console.log("Unable to fetch the user...");
+        }
 
-    // db.collection("tasks").insertMany([
-    //     {
-    //         description: "Clean the house",
-    //         completed: true
-    //     },
-    //     {
-    //         description: "Grocery shopping",
-    //         completed: false,
-    //     },
-    //     {
-    //         description: "Laundry",
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return ("Tasks were not inserted successfully...");
-    //     }
-    //     console.log(result.ops);
-    // });
+        console.log(user);
+    });
+    
+    // We get back a cursor, not a callback function
+    db.collection("users").find({ age: 32 }).toArray((error, users) => {
+        console.log(users);
+    });
+
+    // We get back a cursor, not a callback function
+    db.collection("users").find({ age: 32 }).count((error, count) => {
+        console.log(count);
+    });
+
+    db.collection("tasks").findOne({ _id: new ObjectID("5f817be442764f3ab41a83ad") }, (error, task) => {
+        if (error) {
+            return console.log("Unable to fetch the task...");
+        }
+        console.log(task);
+    });
+
+    db.collection("tasks").find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
+    });
 });
 
