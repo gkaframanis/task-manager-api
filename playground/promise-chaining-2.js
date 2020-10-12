694110c9dde7662ddcf8b96f1a6b5960545bb655
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-12 11:48:56 +0300

    Integrating async/await to playground example with promises

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -5,11 +5,23 @@ const Task = require("../src/models/task");
 // 5f82cc75a3837c65969651b1
 // Model.findByIdAndDelete()
 
-Task.findByIdAndDelete("5f82cc75a3837c65969651b1").then((task) => {
-    console.log(task);
-    return Task.countDocuments({completed: false});
-}).then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete("5f82cc75a3837c65969651b1").then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({completed: false});
+// }).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false});
+    return count;
+};
+
+deleteTaskAndCount("5f82c239163b203deb76518c").then((count) => {
+    console.log(count);
 }).catch((error) => {
     console.log(error);
 });
