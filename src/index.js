4e6b0a74b27dac8b1fc06c4aabb1e5f1a982e5a7
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-12 12:24:07 +0300

    Integrating async/await to the routes in the src/index.js

--- src/index.js
+++ src/index.js
@@ -12,68 +12,81 @@ const port = process.env.PORT || 3000;
 // Automatically parses any incoming json to on object to access it to our request handler.
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
     // We get the data from an http request through postman.
     const user = new User(req.body);
 
-    user.save().then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-    }).catch((error)=> {
-        // To change the status when we encounter an error.
+
+    } catch (error) {
         res.status(400).send(error);
-    });
+    }
 });
 
-app.get("/users", (req, res) => {
-    User.find({}).then((users) => {
+app.get("/users", async (req, res) => {
+
+    try {
+        const users = await User.find({});
         res.send(users);
-    }).catch((error) => {
+        
+    } catch (error) {
         res.status(500).send();
-    });
+    }
 });
 
 // We use route parameter
 // req.params returns an object with key the param and value the value we pass at the url.
-app.get("/users/:id", (req, res) => {
-    User.findById({_id: req.params.id }).then((user) => {
+app.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const user = await User.findById(_id);
+
         if (!user) {
             return res.status(404).send();
         }
+
         res.send(user);
-    }).catch((error) => {
-        res.status(500).send();
-    });
+    } catch (error) {
+        res.status(500).send();  
+    }
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => {
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error);
-    });
+    }
 });
 
-app.get("/tasks", (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks);
-    }).catch((error) => {
-        res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);  
+    } catch (error) {
+        res.status(500).send();   
+    }
 });
 
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
     const _id = req.params.id;
-    // mongoose converts strings automatically to ObjectIDs.
-    Task.findById(_id).then((task) => {
-        if (!task) {
+
+    try {
+        const task = await Task.findById(_id);
+        if(!task) {
             return res.status(404).send();
         }
         res.send(task);
-    }).catch((error) => {
-        res.status(500).send();
-    });
+    } catch (error) {
+        res.send(500).send();
+    }
 });
 
 app.listen(port, () => {
