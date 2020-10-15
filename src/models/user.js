d00895eccf85fe3eef687cc798938a1c3248d2be
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-15 11:36:05 +0300

    Use the authentication tokens at login or sign up

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,8 +40,26 @@ const userSchema = new mongoose.Schema({
                 throw new Error("Age must me a positive number...");
             }
         }
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    
+    return token;
+};
 
 // We can access it directly to the model.
 userSchema.statics.findByCredentials = async (email, password) => {
