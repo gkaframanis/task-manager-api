6e55bbf072c9c11942e23d0ffddd065b86798ef4
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-16 10:17:46 +0300

    Adding authentication to the other user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -57,25 +57,8 @@ router.get("/users/me", auth, async (req, res) => {
     res.send(req.user);
 });
 
-// We use route parameter
-// req.params returns an object with key the param and value the value we pass at the url.
-router.get("/users/:id", async (req, res) => {
-    const _id = req.params.id;
 
-    try {
-        const user = await User.findById(_id);
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (error) {
-        res.status(500).send();  
-    }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ["name", "email", "password", "age"];
     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -85,19 +68,25 @@ router.patch("/users/:id", async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id);
+        updates.forEach(update => req.user[update] = req.body[update]);
 
-        updates.forEach(update => user[update] = req.body[update]);
+        await req.user.save();
 
-        await user.save();
-
-        if(!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
+        res.send(req.user);
     } catch (error) {
         // Handling the validation errors
         res.status(400).send(error);
+    }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+    try {
+        // const user = User.findByIdAndDelete(req.user._id);
+
+        await req.user.remove();
+        res.send(req.user);
+    } catch (error) {
+        res.status(500).send();
     }
 });
 
