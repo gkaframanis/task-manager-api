643bd54e8721360f57b4c1b30e00c6ef18bac8c1
Grigorios Kaframanis <gkaframanis@gmail.com>
2020-10-18 13:44:17 +0300

    Welcome and cancelation emails to the users

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = "SG.jEXowQBJT_uFGJ4j3MVswg.HwKMwlNf5Is2v4WLQU9f-sINV9j0b_
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-    to: "gkaframanis@gmail.com",
-    from: "gregotel7@gmail.com",
-    subject: "This is my first creation",
-    text: "I hope this one actually get to you."
-});
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "gkaframanis@gmail.com",
+        subject: "Thanks for joining in!",
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+    });
+};
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "gkaframanis@gmail.com",
+        subject: "Sorry to see you go!",
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
+    });
+};
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+};
